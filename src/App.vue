d014aac4720c2436962dea088e2ac0d32aa89e7d
ErikCuevas03 <ryuekq@gmail.com>
2025-03-22 14:49:04 -0600

    task #0.5 Agregando Carrusel

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Navbar from "./components/Navbar.vue"
 import descripcion from "./components/descripcion.vue";
+import carrusel from "./components/carrusel.vue";
 </script>
 
 <template>
 <Navbar/>
 <descripcion/>
+<carrusel/>
 </template>
 
 <style scoped>
