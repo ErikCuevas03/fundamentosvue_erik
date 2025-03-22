382b655ebaf69c8ad12e3335dca42272266ea940
ErikCuevas03 <ryuekq@gmail.com>
2025-03-22 15:02:29 -0600

    task #06 Agregando formulario

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
 import Navbar from "./components/Navbar.vue"
 import descripcion from "./components/descripcion.vue";
 import carrusel from "./components/carrusel.vue";
+import formulario from "./components/formulario.vue";
 </script>
 
 <template>
-<Navbar/>
-<descripcion/>
-<carrusel/>
+  <Navbar />
+  <descripcion />
+  <carrusel />
+  <formulario />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
