db197ee129e6e3e3da9f810d67496acb17d7a37d
ErikCuevas03 <ryuekq@gmail.com>
2025-03-22 14:30:34 -0600

    task #0.4 Agregando descripcion

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Navbar from "./components/Navbar.vue"
+import descripcion from "./components/descripcion.vue";
 </script>
 
 <template>
 <Navbar/>
+<descripcion/>
 </template>
 
 <style scoped>
